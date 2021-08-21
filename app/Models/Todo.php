23133159ebdd19509f207df9c3627f0df563570e
thoaidm <minhthoai3@gmail.com>
2021-08-21 20:22:03 +0700

    update

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Todo extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'todos';
+    protected $fillable = [
+        'name',
+        'description',
+        'completed'
+    ];
+    public $incrementing = true;
 }
