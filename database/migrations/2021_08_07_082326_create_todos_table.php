5ac21b57f252f8b774663deec23e9bd123b7d63b
thoaidm <minhthoai3@gmail.com>
2021-08-21 20:29:14 +0700

    update

--- database/migrations/2021_08_07_082326_create_todos_table.php
+++ database/migrations/2021_08_07_082326_create_todos_table.php
@@ -17,7 +17,7 @@ class CreateTodosTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
-            $table->boolean('activated');
+            $table->boolean('completed');
             $table->timestamps();
             $table->softDeletes();
         });
